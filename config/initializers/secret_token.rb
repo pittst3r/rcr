e15f7df965ad9a4dd31c3fa6a1b2c2f3c2a7249a
Robbie Pitts <robbie@sweatypitts.com>
2013-04-14 19:19:05 -0400

    Refinery app .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rcr::Application.config.secret_token = '7b1ddf14b1e7e2e6892556fe2be9211157f4ee15a31bbc2c78ae9224cc80c77a4eba5bfd8d4b96ec00ab879996c9b116a8ee80a8ab94dc4cd9489646a38382fc'
